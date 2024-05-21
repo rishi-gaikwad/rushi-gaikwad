a6407e853f4449abef2ca1de7f78f6b2569a57dd
rishi-gaikwad <gaikwadrushikesh2505>
2024-05-22 00:18:29 +0530

    added new project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -46,20 +46,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/rishi-gaikwad/whatsapp-clone" target="_blank" rel="noreferrer">
+                <a href="https://search-git-hub-username.vercel.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>WhatsApp-Clone</h3>
+              <h3>Search-GitHub-Username </h3>
               <p>
-      The WhatsApp Clone - Simple Design project provided valuable experience in frontend development and user interface design. It showcased my ability to create functional and visually appealing web applications using modern frontend technologies.
+                The GitHub Logo SVG project provides users with a scalable version of the GitHub logo in SVG format. It ensures clarity and consistency across digital platforms, allowing easy integration into websites, applications, and other projects. Accessible via GitHub's Brand Assets page, it offers versatility for customization while maintaining brand integrity. Future developments may include updates to the logo design and enhanced branding guidelines.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Next js</li>
+                <li>React js</li>
                 <li>Html</li>
                 <li>Tailwind Css</li>
               </ul>
